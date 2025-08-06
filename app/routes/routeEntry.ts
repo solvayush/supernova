882b0a2bdd688f3061fc21d3c8808285b32f182f
iamayushy <ayush@presolv360.com>
2025-08-06 15:34:35 +0530

    cf workers build

--- app/routes/routeEntry.ts
+++ app/routes/routeEntry.ts
@@ -1,7 +1,14 @@
 import type { IApplicatioRoutes } from "@/types/routes";
 import authRoutes from "./auth";
+import { lazy } from "react";
+const notFound = lazy(() => import("@/pages/404"));
+
 const applicationRoutes: IApplicatioRoutes[] = [
-    ...authRoutes
+    ...authRoutes,
+    {
+        path: "*",
+        component: notFound,
+    }
 ];
 
 export default applicationRoutes
